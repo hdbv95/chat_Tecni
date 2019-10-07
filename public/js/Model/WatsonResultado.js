c33adebe49706d8ea55b00146df7e6e256d08e23
jhonnyalx123 <jhonnyzapat@gmail.com>
2019-10-07 11:21:15 -0500

    leerReglasTecniseguros

--- public/js/Model/WatsonResultado.js
+++ public/js/Model/WatsonResultado.js
@@ -1,13 +1,20 @@
 module.exports =class WatsonResulado{
     
-    constructor (autentificar,usuario,prestamos,token,numeroPrestamo,direcciones,dirID,asignarAsesor){
-        this.autentificar=autentificar
-        this.usuario=usuario
-        this.prestamos=prestamos
-        this.token=token
-        this.numeroPrestamo=numeroPrestamo
-        this.direcciones=direcciones
-        this.dirID=dirID
+    constructor (nombresCotizacion,Provincias,telefono,Ano_Modelo,Monto_Vehiculo,correo,cedula,
+        primaNeta,primaAnual,tasa,cuotas,Coberturas,Exclusiones,asignarAsesor){
+        this.nombresCotizacion=nombresCotizacion
+        this.Provincias=Provincias
+        this.telefono=telefono
+        this.Ano_Modelo=Ano_Modelo
+        this.Monto_Vehiculo=Monto_Vehiculo
+        this.correo=correo
+        this.cedula=cedula
+        this.primaNeta=primaNeta
+        this.primaAnual=primaAnual
+        this.tasa=tasa
+        this.cuotas=cuotas
+        this.Coberturas=Coberturas
+        this.Exclusiones=Exclusiones
         this.asignarAsesor=asignarAsesor
     }
 
