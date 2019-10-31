167648e2178c9934b12a273c95640017449ab890
jhonnyalx123 <jhonnyzapat@gmail.com>
2019-10-31 10:03:33 -0500

    despliegue

--- app.js
+++ app.js
@@ -1,37 +1,21 @@
 var express = require('express');
-var cfenv = require('cfenv');
 var morgan=require('morgan');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var pruebaRutas=require('./public/js/Routes/routesWex');
 var app = express();
-var cors = require('cors') 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-var mongoose=require('mongoose');
 var credencialesWex=require('./public/js/Conexion/credencialesWex');
-var appEnv = cfenv.getAppEnv();
 var Request = require("request");
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(credencialesWex.telegram.key, {polling: true});
 
-var usuarios = [];
-var salas = [];
-
-
-mongoose.connect(credencialesWex.mongo.url,{dbName: credencialesWex.mongo.bd ,useNewUrlParser: true}
-    ).then(()=>{console.log('successfully connected to MongoDB');/*mongod.cfg cambiar bindIp: de 127.0.0.1 a 0.0.0.0 */}).catch(err=>{
-      console.log("error BD");
-      //process.exit();
-    });
 
 
 // trust first proxy 
 app.set('trust proxy', 1) ;
-app.use(cors({origin:[
-    "http://192.168.10.221:4200"
-  ],credentials:true})) 
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
@@ -40,7 +24,7 @@ app.use(session({
 }));
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -64,125 +48,16 @@ app.use(morgan('dev'));
 
 app.use("/vehiculo",pruebaRutas);
 
-io.on('connection', (socket)=>{
-	console.log("USUARIO CONECTADO"+socket.id);
-  
-	  io.emit('salas', salas);
-	  io.emit('session_update', usuarios);
-	  socket.on('update_list',( data )=> 
-	  {               
-		  if(String(data.action) == 'login')
-		  {
-			  var user = { idSocket: socket.id, id: data.id, usuario: data.usuario };
-			  console.log(user)
-			  var comprobar = buscarUsuario(user.id)
-			  //console.log(comprobar)
-			  if(comprobar == false){
-				  console.log("no exis")
-				  usuarios.push(user);
-			  }else{
-				  console.log("si exis")
-				  io.emit('salas', salas);
-				  usuarios.forEach(userb => {
-					  if(userb.id == user.id){
-						  userb.idSocket = socket.id;
-  
-					  }
-					  
-				  });
-			  }
-			  console.log(usuarios)
-  
-		  }
-		  else
-		  {
-			  // Borrar al usuario de las sesiones
-			  var index = fnFindUser(data.id);
-			  
-			  if (index > -1) 
-			  {
-				  usuarios.splice(index, 1);
-			  }
-		  } 
-		  
-		  io.emit('session_update', usuarios);
-		  
-	  });
-	  socket.on('privatechatroom', (data)=> {
-		  console.log(data)
-		  socket.join (data.sala);
-		  var sala = {
-			  sala:String,
-			  idAsesor:String,
-			  cliente:String,
-			  asesor:String
-		  }
-		  sala.sala = data.sala;
-		  sala.idAsesor = data.idAsesor;
-		  sala.cliente = data.usuario;
-		  sala.asesor = data.asesor;
-		  salas.push(sala);
-		  console.log(sala)
-		  io.emit('salas', salas);
-  
-				  });
-	  socket.on('deleteSala',(data)=>{
-		  console.log("antes salas")
-		  console.log(salas)
-		  salas.forEach((s,x) => {
-			  
-			  if(s.sala == data.sala){
-				  socket.leave(data.sala);
-				  salas.splice(x, 1);
-			  }
-		  });
-		  io.emit('salas', salas);
-		  console.log("despues salas")
-		  console.log(salas)
-  
-	  })
-	  socket.on('unirSala', (data)=> {
-	  console.log(data)
-  socket.join (data.sala);
-  
-	  }); 
-		  
-	  socket.on('sendmail',(data)=>
-  {
-	  io.sockets.in(data.sala).emit('new_msg', {msg: data.message,tipo:data.tipo});
-			  console.log(data.sala);
-	  });
-	  socket.on("NuevoMsj",(data)=>{
-	  console.log(data)
-	  socket.emit('Mensaje-recibido',{
-		message:data
-	  })
-	  })
-  
-	  socket.on('disconnect',()=>{
-	  console.log('Usuario desconectado')
-	  })
-  
-  })
-function buscarUsuario(id){
-	var valid = false;
-	usuarios.forEach(user => {
-		if(user.id == id){
-			valid = true;
-		}
-	});
-	return valid;
-}
 // start server on the specified port and binding host
-server.listen(appEnv.port, '0.0.0.0', function() {
-  console.log("server starting on " + appEnv.url);
+server.listen(7000, '0.0.0.0', function() {
+  console.log("server starting on " + 7000);
 });
 
 bot.on('message', msg => {
-	//console.log(msg);
+	console.log(msg);
     Request.post({
         "headers": { "content-type": "application/json" },
-        "url": "http://localhost:6001/vehiculo/enviarMensaje",
+        "url": "http://192.168.10.239:7000/vehiculo/enviarMensaje",
         "body": JSON.stringify({
             "texto": msg.text,
             "id": msg.chat.id
